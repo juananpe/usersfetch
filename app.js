8df770242e0686c9902779800ed07af3c837d80f
Iñigo Salazar <98783979+isalazar16@users.noreply.github.com>
2023-11-20 14:56:06 +0100

    undo

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/styles/css", express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))); // <- This will use the contents of 'bootstrap/dist/css' which is placed in your node_modules folder as if it is in your '/styles/css' directory.
-hdfjasd
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
